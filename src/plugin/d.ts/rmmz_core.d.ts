cabba364d7211a554b21bacabe639ad00c81fec3
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2020-10-12 03:23:36 +0900

    [chg] JsonEXのd.ts定義をよりTypeScriptに適した形に直した。実質的にstatic classと同義。

--- src/plugin/d.ts/rmmz_core.d.ts
+++ src/plugin/d.ts/rmmz_core.d.ts
@@ -2,11 +2,45 @@
 // rmmz_core.js v1.0.0
 //=============================================================================
 /**
- * @namespace JsonEx
+ * @static
+ * @class JsonEx
  * @classdesc The static class that handles JSON with object information.
  */
-declare namespace JsonEx {
-    function stringify(object: any): string;
-    function parse(json: string): any;
-    function makeDeepCopy(object: any): any;
+declare class JsonEx {
+    constructor();
+
+    /**
+     * The maximum depth of objects.
+     *
+     * @type number
+     * @default 100
+     */
+    public static readonly maxDepth: number;
+
+    /**
+     * Converts an object to a JSON string with object information.
+     *
+     * @param {object} object - The object to be converted.
+     * @returns {string} The JSON string.
+     */
+    public static stringify(object: any): string;
+
+    /**
+     * Parses a JSON string and reconstructs the corresponding object.
+     *
+     * @param {string} json - The JSON string.
+     * @returns {object} The reconstructed object.
+     */
+    public static parse(json: string): any;
+
+    /**
+     * Makes a deep copy of the specified object.
+     *
+     * @param {object} object - The object to be copied.
+     * @returns {object} The copied object.
+     */
+    public static makeDeepCopy(object: any): any;
+
+    private static _encode(value: any, depth: number): any;
+    private static _decode(value: any): any;
 }
