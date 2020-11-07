f9469ed1c09826d60a70128e1226157fefa91296
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2020-11-08 04:55:41 +0900

    [add] TouchInputに対応したd.tsを記述。

--- src/plugin/d.ts/rmmz_core.d.ts
+++ src/plugin/d.ts/rmmz_core.d.ts
@@ -177,6 +177,168 @@ declare class Input {
     private static _isEscapeCompatible(keyName: string): boolean;
 }
 
+type TouchInputState = {
+    triggered: boolean;
+    cancelled: boolean;
+    moved: boolean;
+    hovered: boolean;
+    released: boolean;
+    wheelX: number;
+    wheelY: number;
+};
+
+/**
+ * @static
+ * @class TouchInput
+ * @classdesc The static class that handles input data from the mouse and touchscreen.
+ */
+declare class TouchInput {
+    /**
+     * The wait time of the pseudo key repeat in frames.
+     * @type {number}
+     */
+    static keyRepeatWait: number;
+    /**
+     * The interval of the pseudo key repeat in frames.
+     * @type {number}
+     */
+    static keyRepeatInterval: number;
+    /**
+     * The threshold number of pixels to treat as moved.
+     * @type {number}
+     */
+    static movethreshold: number;
+
+    private _mousePressed: boolean;
+    private _screenPressed: boolean;
+    private _pressedTime: number;
+    private _clicked: boolean;
+    private _newState: TouchInputState;
+    private _currentState: TouchInputState;
+    private _x: number;
+    private _y: number;
+    private _triggerX: number;
+    private _triggerY: number;
+    private _moved: boolean;
+    private _date: number;
+
+    constructor();
+
+    public static clear(): void;
+    /**
+     * Updates the touch data.
+     */
+    public static update(): void;
+
+    /**
+     * Initializes the touch system.
+     */
+    public static initialize(): void;
+    /**
+     * Checks whether the mouse button or touchscreen has been pressed and
+     * released at the same position.
+     * @returns {boolean} True if the mouse button or touchscreen is clicked.
+     */
+    public static isClicked(): boolean;
+    /**
+     * Checks whether the mouse button or touchscreen is currently pressed down.
+     * @returns {boolean} True if the mouse button or touchscreen is pressed.
+     */
+    public static isPressed(): boolean;
+    /**
+     * Checks whether the left mouse button or touchscreen is just pressed.
+     * @returns {boolean} True if the mouse button or touchscreen is triggered.
+     */
+    public static isTriggered(): boolean;
+    /**
+     * Checks whether the left mouse button or touchscreen is just pressed
+     * or a pseudo key repeat occurred.
+     * @returns {boolean} True if the mouse button or touchscreen is repeated.
+     */
+    public static isRepeated(): boolean;
+    /**
+     * Checks whether the left mouse button or touchscreen is kept depressed.
+     * @returns {boolean} True if the left mouse button or touchscreen is long-pressed.
+     */
+    public static isLongPressed(): boolean;
+    /**
+     * Checks whether the right mouse button is just pressed.
+     * @returns {boolean} True if the right mouse button is just pressed.
+     */
+    public static isCancelled(): boolean;
+    /**
+     * Checks whether the mouse or a finger on the touchscreen is moved.
+     * @returns {boolean} True if the mouse or a finger on the touchscreen is moved.
+     */
+    public static isMoved(): boolean;
+    /**
+     * Checks whether the mouse is moved without pressing a button.
+     * @returns {boolean} True if the mouse is hovered.
+     */
+    public static isHovered(): boolean;
+    /**
+     * Checks whether the left mouse button or touchscreen is released.
+     * @returns {boolean} True if the mouse button or touchscreen is released.
+     */
+    public static isReleased(): boolean;
+
+    /**
+     * The horizontal scroll amount.
+     * @readonly
+     * @type {number}
+     * @name TouchInput.wheelX
+     */
+    get wheelX(): number;
+    /**
+     * The vertical scroll amount.
+     * @readonly
+     * @type {number}
+     * @name TouchInput.wheelY
+     */
+    get wheelY(): number;
+    /**
+     * The x coordinate on the canvas area of the latest touch event.
+     * @readonly
+     * @type {number}
+     * @name TouchInput.x
+     */
+    get x(): number;
+    /**
+     * The y coordinate on the canvas area of the latest touch event.
+     * @readonly
+     * @type {number}
+     * @name TouchInput.y
+     */
+    get y(): number;
+    /**
+     * The time of the last input in milliseconds.
+     * @readonly
+     * @type {number}
+     * @name TouchInput.date
+     */
+    get date(): number;
+
+    private static _createNewState(): TouchInputState;
+    private static _setupEventHandlers(): void;
+    private static _onMouseDown(event: any): void;
+    private static _onLeftButtonDown(event: any): void;
+    private static _onMiddleButtonDown(): void;
+    private static _onRightButtonDown(event: any): void;
+    private static _onMouseMove(event: any): void;
+    private static _onMouseUp(event: any): void;
+    private static _onWheel(event: any): void;
+    private static _onTouchStart(event: any): void;
+    private static _onTouchMove(event: any): void;
+    private static _onTouchEnd(event: any): void;
+    private static _onTouchCancel(): void;
+    private static _onLostFocus(): void;
+    private static _onTrigger(): void;
+    private static _onCancel(): void;
+    private static _onMove(x: number, y: number): void;
+    private static _onHover(x: number, y: number): void;
+    private static _onRelease(x: number, y: number): void;
+}
+
 /**
  * @static
  * @class JsonEx
